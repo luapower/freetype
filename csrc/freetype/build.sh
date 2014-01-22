cfd0ffe58d02f8a050148a2bc331d0a38fa3f038
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:50:23 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/freetype/build.sh
+++ csrc/freetype/build.sh
@@ -5,7 +5,7 @@
 [[ "$(uname)" == *Linux* ]] && libfile=../../bin/linux32/libfreetype.so
 [[ "$(uname)" == *MINGW* ]] && libfile=../../bin/mingw32/freetype.dll
 
-args=(gcc $CFLAGS -shared -o "$libfile" -Icustom -Iinclude -DFT2_BUILD_LIBRARY
+args=(gcc -O2 -s -static-libgcc -shared -o "$libfile" -Icustom -Iinclude -DFT2_BUILD_LIBRARY
 
       src/base/ftsystem.c
       src/base/ftinit.c
