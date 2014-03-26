2f00891f8952018c50b85b6ed9e060a0b9e28f7f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:06:54 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/freetype/build-osx32.sh
+++ csrc/freetype/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 `./files.sh` -shared -o ../../bin/osx32/libfreetype.dylib -Icustom -Iinclude -DFT2_BUILD_LIBRARY
+gcc -arch i386 -O2 `./files.sh` -shared -install_name @loader_path/libfreetype.dylib -o ../../bin/osx32/libfreetype.dylib -Icustom -Iinclude -DFT2_BUILD_LIBRARY
--- csrc/freetype/build-osx64.sh
+++ csrc/freetype/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 `./files.sh` -shared -o ../../bin/osx64/libfreetype.dylib -Icustom -Iinclude -DFT2_BUILD_LIBRARY
+gcc -arch x86_64 -O2 `./files.sh` -shared -install_name @loader_path/libfreetype.dylib -o ../../bin/osx64/libfreetype.dylib -Icustom -Iinclude -DFT2_BUILD_LIBRARY
