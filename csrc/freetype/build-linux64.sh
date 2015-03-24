4eb176557e9170c07d43336296677962961708a7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:58:38 +0200

    unimportant

--- csrc/freetype/build-linux64.sh
+++ csrc/freetype/build-linux64.sh
@@ -1,1 +1,1 @@
-P=linux64 C=-fPIC L="-s -static-libgcc" D=libfreetype.so A=libfreetype.a ./build.sh
+P=linux64 C="-fPIC -include _memcpy.h -U_FORTIFY_SOURCE" L="-s -static-libgcc" D=libfreetype.so A=libfreetype.a ./build.sh
